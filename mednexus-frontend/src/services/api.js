13842f5a9d40f99c4e1a568304a84eeff5e19040
sarthaksharmacs22 <sarthaksharma1237@gmail.com>
2026-03-27 14:31:48 +0530

    fixed baseURL removed /docs

--- mednexus-frontend/src/services/api.js
+++ mednexus-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://edc8-34-63-184-199.ngrok-free.app/docs";
+const API_BASE_URL = "https://edc8-34-63-184-199.ngrok-free.app/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
